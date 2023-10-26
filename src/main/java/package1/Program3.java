40e6e2ec8e3421301b172cd2643f3c391471b496
amisha92501ayush <kumarraju92501@gmail.com>
2023-10-26 21:22:04 +0530

    Selenium and flipkart has been commited

--- src/main/java/package1/Program3.java
+++ src/main/java/package1/Program3.java
@@ -1,11 +1,24 @@
 package package1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Program3 {
 	@Test
-	public static void demo1() {
+	public static void demo1() throws InterruptedException {
 		System.out.println("This is demo3 method");
+		System.out.println("This is demo3 method");
+		System.out.println("This is demo3 method");
+		System.out.println("This is demo3 method");
+		System.out.println("This is demo3 method");
+		System.out.println("This is demo3 method");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.selenium.dev/");
+		Thread.sleep(10000);
+		driver.get("https://www.flipkart.com/");
+		
 	}
 	
 }
